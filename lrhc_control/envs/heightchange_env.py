fixed missing reciprocal for reward computation

lrhc_control/envs/heightchange_env.py
@@ -56,7 +56,7 @@ class LRhcHeightChange(LRhcTrainingEnvBase):
         # controllers failure penalty
         rhc_fail_penalty = self._rhc_status.fails.get_torch_view(gpu=self._use_gpu)
 
-        self._rewards[:, :] = h_error + rhc_cost + rhc_const_viol + rhc_fail_penalty
+        self._rewards[:, :] = torch.reciprocal(h_error + rhc_cost + rhc_const_viol + rhc_fail_penalty)
 
     def _get_observations(self):
                 

lrhc_control/training_algs/ppo.py
@@ -157,7 +157,7 @@ class CleanPPO():
 
                 with torch.no_grad():
                     # calculate approx_kl http://joschu.net/blog/kl-approx.html
-                    old_approx_kl = (-logratio).mean()
+                    # old_approx_kl = (-logratio).mean()
                     approx_kl = ((ratio - 1) - logratio).mean()
                     clipfracs += [((ratio - 1.0).abs() > self._clip_coef).float().mean().item()]
 