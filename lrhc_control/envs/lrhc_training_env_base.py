fixed truncations and terminations computation bug

lrhc_control/envs/lrhc_training_env_base.py
@@ -74,20 +74,6 @@ class LRhcTrainingEnvBase():
         self._agent_refs = None
 
         self._step_counter = 0
-        self._episode_counters = StepCounterEpisode(namespace=self._namespace,
-                            n_envs=self._n_envs,
-                            reset_n_steps=self._episode_length,
-                            obs_dim=self._obs_dim,
-                            obs_names=self._get_obs_names(),
-                            env_names=None,
-                            is_server=True,
-                            verbose=self._verbose,
-                            vlevel=self._vlevel,
-                            safe=True,
-                            force_reconnection=True,
-                            with_gpu_mirror=self._use_gpu,
-                            fill_value=0) # handles step counter through episodes and through envs
-        self._episode_counters.reset(all=True)
 
         self._n_envs = 0
 
@@ -97,6 +83,7 @@ class LRhcTrainingEnvBase():
 
         self._perf_timer = PerfSleep()
 
+        self._episode_counters = None
         self._obs = None
         self._actions = None
         self._tot_rewards = None
@@ -203,11 +190,19 @@ class LRhcTrainingEnvBase():
         self._rewards.reset()
         self._tot_rewards.reset()
 
-        self._terminations.zero_()
-        self._truncations.zero_()
+        self._terminations.reset()
+        self._truncations.reset()
+
+        self._episode_counters.reset(all=True)
+
+        self._remote_reset()
 
         self._get_observations()
         self._clamp_obs() # to avoid bad things
+    
+    def _remote_reset(self):
+
+        pass
 
     def close(self):
         
@@ -217,12 +212,17 @@ class LRhcTrainingEnvBase():
         self._rhc_status.close()
         
         self._remote_stepper.close()
+        
+        self._episode_counters.close()
 
         # closing env.-specific shared data
         self._obs.close()
         self._actions.close()
         self._rewards.close()
         self._tot_rewards.close()
+
+        self._terminations.close()
+        self._truncations.close()
 
     def get_last_obs(self):
     
@@ -355,8 +355,6 @@ class LRhcTrainingEnvBase():
         # the current step. An episode termination could occur based on predefined conditions
         # in the environment, such as reaching a goal or exceeding a time limit.
 
-        device = "cuda" if self._use_gpu else "cpu"
-
         self._terminations = Terminations(namespace=self._namespace,
                             n_envs=self._n_envs,
                             is_server=True,
@@ -451,7 +449,20 @@ class LRhcTrainingEnvBase():
                             safe=self._safe_shared_mem)
         self._remote_stepper.run()
         self._remote_stepper.training_env_ready()
-    
+        
+        # episode steps counters 
+        self._episode_counters = StepCounterEpisode(namespace=self._namespace,
+                            n_envs=self._n_envs,
+                            reset_n_steps=self._episode_length,
+                            is_server=True,
+                            verbose=self._verbose,
+                            vlevel=self._vlevel,
+                            safe=True,
+                            force_reconnection=True,
+                            with_gpu_mirror=self._use_gpu) # handles step counter through episodes and through envs
+        self._episode_counters.run()
+        self._episode_counters.reset(all=True)
+
         # debug data servers
         traing_env_param_dict = {}
         traing_env_param_dict["use_gpu"] = self._use_gpu
@@ -574,7 +585,7 @@ class LRhcTrainingEnvBase():
 
         terminations = self._terminations.get_torch_view(gpu=self._use_gpu)
         # handle episodes termination
-        terminations[:, :] = self._episode_counters.reset(env_indxs=self._episode_counters.to_be_reset())
+        terminations[:, :] = self._episode_counters.finished_episodes() 
 
         if self._use_gpu:
             # from GPU to CPU 
@@ -586,9 +597,15 @@ class LRhcTrainingEnvBase():
         
         self._episode_counters.increment() # first increment counters
 
-        self._check_truncations()
+        self._check_truncations() 
         self._check_terminations()
 
+        either_truncated_or_terminated = torch.logical_or(self._terminations.get_torch_view(gpu=self._use_gpu),
+                                        self._truncations.get_torch_view(gpu=self._use_gpu))
+
+        self._episode_counters.reset(to_be_reset=either_truncated_or_terminated)
+        
+        # reset counter if either terminated
         if self._is_debug:
             
             self._debug()

lrhc_control/utils/shared_data/training_env.py
@@ -414,7 +414,7 @@ class Terminations(SharedDataView):
 
     def __init__(self,
             namespace: str,
-            n_rows: int = None, 
+            n_envs: int = None, 
             is_server = False, 
             verbose: bool = False, 
             vlevel: VLevel = VLevel.V0,
@@ -428,7 +428,7 @@ class Terminations(SharedDataView):
             super().__init__(namespace = namespace,
                 basename = basename,
                 is_server = is_server, 
-                n_rows = n_rows, 
+                n_rows = n_envs, 
                 n_cols = 1, 
                 verbose = verbose, 
                 vlevel = vlevel,
@@ -438,13 +438,49 @@ class Terminations(SharedDataView):
                 with_gpu_mirror=with_gpu_mirror,
                 fill_value = fill_value)
 
+    def reset(self):
+
+        self.to_zero()
+
+class Truncations(SharedDataView):
+
+    def __init__(self,
+            namespace: str,
+            n_envs: int = None, 
+            is_server = False, 
+            verbose: bool = False, 
+            vlevel: VLevel = VLevel.V0,
+            safe: bool = True,
+            force_reconnection: bool = False,
+            with_gpu_mirror: bool = False,
+            fill_value = 0):
+            
+            basename = "Truncations"
+    
+            super().__init__(namespace = namespace,
+                basename = basename,
+                is_server = is_server, 
+                n_rows = n_envs, 
+                n_cols = 1, 
+                verbose = verbose, 
+                vlevel = vlevel,
+                safe = safe, # boolean operations are atomic on 64 bit systems
+                dtype=sharsor_dtype.Bool,
+                force_reconnection=force_reconnection,
+                with_gpu_mirror=with_gpu_mirror,
+                fill_value = fill_value)
+
+    def reset(self):
+
+        self.to_zero()
+        
 class StepCounterEpisode(SharedDataBase):
 
     class StepCounter(SharedDataView):
 
         def __init__(self,
                 namespace: str,
-                n_rows: int = None, 
+                n_envs: int = None, 
                 is_server = False, 
                 verbose: bool = False, 
                 vlevel: VLevel = VLevel.V0,
@@ -458,7 +494,7 @@ class StepCounterEpisode(SharedDataBase):
                 super().__init__(namespace = namespace,
                     basename = basename,
                     is_server = is_server, 
-                    n_rows = n_rows, 
+                    n_rows = n_envs, 
                     n_cols = 1, 
                     verbose = verbose, 
                     vlevel = vlevel,
@@ -471,7 +507,7 @@ class StepCounterEpisode(SharedDataBase):
     def __init__(self,
                 namespace: str,
                 reset_n_steps: int = None,
-                n_rows: int = None, 
+                n_envs: int = None, 
                 is_server = False, 
                 verbose: bool = False, 
                 vlevel: VLevel = VLevel.V0,
@@ -485,8 +521,7 @@ class StepCounterEpisode(SharedDataBase):
 
         self._step_counter = self.StepCounter(namespace = namespace,
                     is_server = is_server, 
-                    n_rows = n_rows, 
-                    n_cols = 1, 
+                    n_envs = n_envs, 
                     verbose = verbose, 
                     vlevel = vlevel,
                     safe = safe, # boolean operations are atomic on 64 bit systems
@@ -502,6 +537,10 @@ class StepCounterEpisode(SharedDataBase):
 
         # copy from cpu to shared memory
         self._step_counter.synch_all(read=False, wait=True)
+
+    def is_running(self):
+
+        return self._step_counter.is_running()
 
     def increment(self):
         
@@ -527,17 +566,24 @@ class StepCounterEpisode(SharedDataBase):
 
         self._step_counter.run()
 
+        device = "cuda" if self._using_gpu else "cpu"
+
+        self._to_be_reset = torch.full(size=(self._step_counter.n_rows, 1), 
+                                fill_value=False,
+                                dtype=torch.bool,
+                                device=device)
+
     def close(self):
 
         self._step_counter.close()
 
-    def _reset_flags(self):
+    def finished_episodes(self):
 
         return self.get() > self._reset_n_steps
     
     def to_be_reset(self):
         
-        to_be_reset = torch.nonzero(self._reset_flags().squeeze()).squeeze()
+        self._to_be_reset[:, :] = torch.nonzero(self.finished_episodes().squeeze()).squeeze()
 
         if to_be_reset.shape[0] == 0:
 
@@ -548,12 +594,12 @@ class StepCounterEpisode(SharedDataBase):
             return to_be_reset
     
     def reset(self,
-        env_indxs: torch.Tensor = None,
+        to_be_reset: torch.Tensor = None,
         all: bool = False):
 
-        if env_indxs is not None:
+        if to_be_reset is not None:
 
-            self.get()[env_indxs, :] = 0
+            self.get()[to_be_reset.squeeze(), :] = 0
 
         if all:
 
@@ -561,32 +607,4 @@ class StepCounterEpisode(SharedDataBase):
 
         self._write()
 
-        return self._reset_flags()
-
-class Truncations(SharedDataView):
-
-    def __init__(self,
-            namespace: str,
-            n_rows: int = None, 
-            is_server = False, 
-            verbose: bool = False, 
-            vlevel: VLevel = VLevel.V0,
-            safe: bool = True,
-            force_reconnection: bool = False,
-            with_gpu_mirror: bool = False,
-            fill_value = 0):
-            
-            basename = "Truncations"
-    
-            super().__init__(namespace = namespace,
-                basename = basename,
-                is_server = is_server, 
-                n_rows = n_rows, 
-                n_cols = 1, 
-                verbose = verbose, 
-                vlevel = vlevel,
-                safe = safe, # boolean operations are atomic on 64 bit systems
-                dtype=sharsor_dtype.Bool,
-                force_reconnection=force_reconnection,
-                with_gpu_mirror=with_gpu_mirror,
-                fill_value = fill_value)
+        return self._to_be_reset