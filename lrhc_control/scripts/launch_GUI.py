aae5fecec8bfd4387448c90e55eb3837da778511
AndrePatri <andreapatrizi1b6e6@gmail.com>
2024-03-12 16:52:07 +0100

    added arguments to GUI

--- lrhc_control/scripts/launch_GUI.py
+++ lrhc_control/scripts/launch_GUI.py
@@ -19,18 +19,24 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Set CPU affinity for the script.")
     parser.add_argument('--cores', nargs='+', type=int, help='List of CPU cores to set affinity to')
     parser.add_argument('--ns', type=str, help='Namespace to be used for cluster shared memory')
-
+    parser.add_argument('--dt_data', type=float, help='Dt at which data on shared memory is sampled',
+                default=0.01)
+    parser.add_argument('--dt_plot', type=float, help='Dt at which plots are updated',
+                default=0.5)
+    parser.add_argument('--w_length', type=float, help='length [s] of the data window into the past',
+                default=5.0)
+    
     args = parser.parse_args()
     
     # Set CPU affinity if cores are provided
     if args.cores:
         set_affinity(args.cores)
 
-    data_update_dt = 0.01
-    plot_update_dt = 0.1
+    data_update_dt = args.dt_data
+    plot_update_dt = args.dt_plot
 
-    window_length = 10.0
-    window_buffer_factor = 2
+    window_length = args.w_length
+    window_buffer_factor = 1
 
     cluster_debugger = RtClusterDebugger(data_update_dt=data_update_dt,
                             plot_update_dt=plot_update_dt,
