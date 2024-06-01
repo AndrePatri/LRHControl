43b9bc7ee1d276d00eb010541d4f49638a66ae1f
AndrePatri <andreapatrizi1b6e6@gmail.com>
2024-06-01 09:44:36 +0200

    just an utility script to record rhc2ros bridge topic for remote inspection

---
+++ lrhc_control/scripts/launch_rosbag.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /opt/ros/humble/setup.bash
+cd $HOME/training_data
+ros2 bag record /RHCViz_kyon0_HandShake /RHCViz_kyon0_hl_refs /RHCViz_kyon0_rhc_actuated_jointnames /RHCViz_kyon0_rhc_q /RHCViz_kyon0_rhc_refs /RHCViz_kyon0_robot_actuated_jointnames /RHCViz_kyon0_robot_q 
