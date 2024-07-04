c6c2b1d3111862a00a9ce34fd23a64630ef09e23
AndrePatri <andreapatrizi1b6e6@gmail.com>
2024-07-04 10:09:47 +0200

    better argparse usage

--- lrhc_control/scripts/launch_train_env.py
+++ lrhc_control/scripts/launch_train_env.py
@@ -25,23 +25,30 @@ if __name__ == "__main__":
     parser.add_argument('--run_name', type=str, help='Name of training run', default="LRHCTraining")
     parser.add_argument('--ns', type=str, help='Namespace to be used for shared memory')
     parser.add_argument('--drop_dir', type=str, help='Directory root where all run data will be dumped')
-    parser.add_argument('--dump_checkpoints', action='store_true', help='Whether to dummp model checkpoints during training')
-    parser.add_argument('--eval', action='store_true', help='If set, evaluates policy instead of training')
     parser.add_argument('--n_evals', type=int, help='N. of evaluation rollouts to be performed', default=None)
     parser.add_argument('--n_timesteps', type=int, help='Toal n. of timesteps for each evaluation rollout', default=None)
     parser.add_argument('--mpath', type=str, help='Model path to be used for policy evaluation',default=None)
-    parser.add_argument('--use_cpu', action='store_true', help='If set, all the training (data included) will be perfomed on CPU')
     parser.add_argument('--comment', type=str, help='Any useful comment associated with this run',default="")
     parser.add_argument('--seed', type=int, help='seed', default=1)
-    parser.add_argument('--disable_db', action='store_true', help='Whether to disable local data logging for the algorithm (reward metrics, etc..)')
-    parser.add_argument('--disable_env_db', action='store_true', help='Whether to disable env db data logging on \
-                        shared mem (e.g.reward metrics are not available for reading anymore)')
-    parser.add_argument('--disable_rmdb', action='store_true', help='Whether to disable remote debug (e.g. data logging on remote servers)')
-    parser.add_argument('--override_agent_refs', action='store_true', help='Whether to override automatically generated agent refs (useful for debug)')
+    
     parser.add_argument('--timeout_ms', type=int, help='connection timeout after which the script self-terminates', default=60000)
 
+    parser.add_argument('--eval', action=argparse.BooleanOptionalAction, default=False, help='Whether to perform an evaluation run')
+    parser.add_argument('--dump_checkpoints', action=argparse.BooleanOptionalAction, default=True, help='Whether to dump model checkpoints during training')
+    parser.add_argument('--use_cpu', action=argparse.BooleanOptionalAction, default=False, help='If set, all the training (data included) will be perfomed on CPU')
+    parser.add_argument('--db', action=argparse.BooleanOptionalAction, default=True, help='Whether to enable local data logging for the algorithm (reward metrics, etc..)')
+    parser.add_argument('--env_db', action=argparse.BooleanOptionalAction, default=True, help='Whether to enable env db data logging on \
+                            shared mem (e.g.reward metrics are not available for reading anymore)')
+    parser.add_argument('--rmdb', action=argparse.BooleanOptionalAction, default=True, help='Whether to enable remote debug (e.g. data logging on remote servers)')
+    parser.add_argument('--obs_norm', action=argparse.BooleanOptionalAction, default=True, help='Whether to enable the use of running normalizer in agent')
+
+    parser.add_argument('--override_agent_refs', action=argparse.BooleanOptionalAction, default=False, \
+                help='Whether to override automatically generated agent refs (useful for debug)')
+    parser.add_argument('--anomaly_detect', action=argparse.BooleanOptionalAction, default=False, \
+                help='Whether to override automatically generated agent refs (useful for debug)')
+
     args = parser.parse_args()
-    
+
     # Set CPU affinity if cores are provided
     if args.cores:
         set_affinity(args.cores)
@@ -50,7 +57,7 @@ if __name__ == "__main__":
                     verbose=True,
                     vlevel=VLevel.V2,
                     use_gpu=not args.use_cpu,
-                    debug=not args.disable_env_db,
+                    debug=args.env_db,
                     override_agent_refs=args.override_agent_refs,
                     timeout_ms=args.timeout_ms)
     
@@ -65,14 +72,14 @@ if __name__ == "__main__":
     for i in range(len(sim_info_keys)):
         sim_data[sim_info_keys[i]] = sim_info_data[i]
     
-    algo = PPO(env=env, 
-            debug=not args.disable_db, 
-            remote_db=not args.disable_rmdb,
+    #algo = PPO(env=env, 
+    #        debug=not args.disable_db, 
+    #        remote_db=not args.disable_rmdb,
+    #        seed=args.seed)
+    algo = SAC(env=env, 
+            debug=args.db, 
+            remote_db=args.rmdb,
             seed=args.seed)
-    # algo = SAC(env=env, 
-    #         debug=not args.disable_db, 
-    #         remote_db=not args.disable_rmdb,
-    #         seed=args.seed)
     algo.setup(run_name=args.run_name, 
         verbose=True,
         drop_dir_name=args.drop_dir,
@@ -83,7 +90,7 @@ if __name__ == "__main__":
         n_evals=args.n_evals,
         n_timesteps_per_eval=args.n_timesteps,
         dump_checkpoints=args.dump_checkpoints,
-        norm_obs=True)
+        norm_obs=args.obs_norm)
 
     try:
         while not algo.is_done():
