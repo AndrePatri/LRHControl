ad2e000508c6f25cdc299acc4140e9492dc1fea0
AndrePatri <andreapatrizi1b6e6@gmail.com>
2024-07-14 22:23:23 +0200

    added sim time arg

--- lrhc_control/scripts/launch_rosbag.sh
+++ lrhc_control/scripts/launch_rosbag.sh
@@ -2,4 +2,4 @@
 
 source /opt/ros/humble/setup.bash
 cd $HOME/training_data
-ros2 bag record /RHCViz_kyon0_HandShake /RHCViz_kyon0_hl_refs /RHCViz_kyon0_rhc_actuated_jointnames /RHCViz_kyon0_rhc_q /RHCViz_kyon0_rhc_refs /RHCViz_kyon0_robot_actuated_jointnames /RHCViz_kyon0_robot_q 
+ros2 bag record --use-sim-time /RHCViz_kyon0_HandShake /RHCViz_kyon0_hl_refs /RHCViz_kyon0_rhc_actuated_jointnames /RHCViz_kyon0_rhc_q /RHCViz_kyon0_rhc_refs /RHCViz_kyon0_robot_actuated_jointnames /RHCViz_kyon0_robot_q 
