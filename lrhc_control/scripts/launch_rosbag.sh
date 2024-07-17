7e011d731bedd9c9da70097663f314108f0e8271
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-07-17 18:23:04 +0200

    Signed-off-by: Andrea Patrizi <andreapatrizi1b6e6@gmail.com>

--- lrhc_control/scripts/launch_rosbag.sh
+++ lrhc_control/scripts/launch_rosbag.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+# Function to display usage information
+usage() {
+    echo "Usage: $0 --ns <namespace>"
+    exit 1
+}
+
+# Check if the argument is provided
+if [ "$#" -ne 2 ] || [ "$1" != "--ns" ]; then
+    usage
+fi
+
+# Extract the namespace from the arguments
+NAMESPACE=$2
+
+# Source ROS setup
 source /opt/ros/humble/setup.bash
+
+# Change to the training data directory
 cd $HOME/training_data
-ros2 bag record --use-sim-time /RHCViz_kyon0_HandShake /RHCViz_kyon0_hl_refs /RHCViz_kyon0_rhc_actuated_jointnames /RHCViz_kyon0_rhc_q /RHCViz_kyon0_rhc_refs /RHCViz_kyon0_robot_actuated_jointnames /RHCViz_kyon0_robot_q 
+
+# Define the topics with the namespace replaced
+TOPICS=(
+    "/RHCViz_${NAMESPACE}_HandShake"
+    "/RHCViz_${NAMESPACE}_hl_refs"
+    "/RHCViz_${NAMESPACE}_rhc_actuated_jointnames"
+    "/RHCViz_${NAMESPACE}_rhc_q"
+    "/RHCViz_${NAMESPACE}_rhc_refs"
+    "/RHCViz_${NAMESPACE}_robot_actuated_jointnames"
+    "/RHCViz_${NAMESPACE}_robot_q"
+)
+
+# Record the topics
+ros2 bag record --use-sim-time "${TOPICS[@]}"
