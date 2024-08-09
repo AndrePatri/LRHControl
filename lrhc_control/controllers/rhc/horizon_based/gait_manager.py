9dc3b38d0448ae34c8c798d6091fef333e274c46
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-08-09 17:10:29 +0200

    removed freg timelines

--- lrhc_control/controllers/rhc/horizon_based/gait_manager.py
+++ lrhc_control/controllers/rhc/horizon_based/gait_manager.py
@@ -18,7 +18,6 @@ class GaitManager:
         self.phase_manager = phase_manager
 
         self._contact_timelines = dict()
-        self._f_reg_timelines = dict()
 
         if injection_node is None:
             self._injection_node = round(self.task_interface.prb.getNNodes()/2.0)
@@ -26,17 +25,9 @@ class GaitManager:
             self._injection_node = injection_node
 
         self._timeline_names = []
-        self._freg_timeline_names = []
         for contact_name, timeline_name in contact_map.items():
             self._contact_timelines[contact_name] = self.phase_manager.getTimelines()[timeline_name]
-            self._f_reg_timelines[contact_name] = None
-            try:
-                self._f_reg_timelines[contact_name] = self.phase_manager.getTimelines()[timeline_name+"_f_reg"]
-            except:
-                pass
-            
             self._timeline_names.append(contact_name)
-            self._freg_timeline_names.append(contact_name)
 
         # self.zmp_timeline = self.phase_manager.getTimelines()['zmp_timeline']
 
@@ -47,17 +38,8 @@ class GaitManager:
     def add_stand(self, timeline_name):
         timeline = self._contact_timelines[timeline_name]
         timeline.addPhase(timeline.getRegisteredPhase(f'stance_{timeline_name}_short'))
-        # f reg
-        freg_timeline = self._f_reg_timelines[timeline_name]
-        if freg_timeline is not None:
-            freg_timeline.addPhase(freg_timeline.getRegisteredPhase(f'freg_{timeline_name}_short'))
     
     def add_flight(self, timeline_name):
         timeline = self._contact_timelines[timeline_name]
         timeline.addPhase(timeline.getRegisteredPhase(f'flight_{timeline_name}'), 
             pos=self._injection_node, absolute_position=True)
-        # f reg
-        freg_timeline = self._f_reg_timelines[timeline_name]
-        if freg_timeline is not None:
-            freg_timeline.addPhase(freg_timeline.getRegisteredPhase(f'freg_{timeline_name}_empty'),
-                pos=self._injection_node, absolute_position=True)
