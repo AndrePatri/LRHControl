56d08e9c4c7b4b39529a81c8ae2f4ae0d324b4eb
Andrea Patrizi <andrepatri@hhcm-pilot-ws-2404.iit.local>
2024-08-27 13:01:15 +0200

    added some regularization on vertical velocity

--- lrhc_control/envs/step_adaptation_baseline.py
+++ lrhc_control/envs/step_adaptation_baseline.py
@@ -107,7 +107,7 @@ class StepAdaptationBaseline(LRhcTrainingEnvBase):
                             fill_value=0.0) 
         self._task_err_weights[0, 0] = 1.0
         self._task_err_weights[0, 1] = 1.0
-        self._task_err_weights[0, 2] = 0.0
+        self._task_err_weights[0, 2] = 0.1
         self._task_err_weights[0, 3] = 0.0
         self._task_err_weights[0, 4] = 0.0
         self._task_err_weights[0, 5] = 0.0
