e58338dfb3057d3c665b59d2bf596a4e6862487b
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-09-16 17:13:45 +0200

    Signed-off-by: Andrea Patrizi <andreapatrizi1b6e6@gmail.com>

--- lrhc_control/scripts/launch_rhc_keybrd_cmds.py
+++ lrhc_control/scripts/launch_rhc_keybrd_cmds.py
@@ -1,4 +1,4 @@
-from control_cluster_bridge.utilities.keyboard_cmds import RhcRefsFromKeyboard
+from control_cluster_bridge.utilities.keyboard_cmds import RefsFromKeyboard
 
 import argparse
 
@@ -8,7 +8,17 @@ if __name__ == "__main__":
     parser.add_argument('--ns', type=str, help='Namespace to be used for shared memory')
     args = parser.parse_args()
     
-    keyb_cmds = RhcRefsFromKeyboard(namespace=args.ns, 
+    from control_cluster_bridge.utilities.shared_data.rhc_data import RhcRefs
+    from SharsorIPCpp.PySharsorIPC import VLevel
+
+    shared_refs= RhcRefs(namespace=args.ns,
+        is_server=False, 
+        safe=False, 
+        verbose=True,
+        vlevel=VLevel.V2)
+
+    keyb_cmds = RefsFromKeyboard(namespace=args.ns, 
+                            shared_refs=shared_refs,
                             verbose=True)
 
     keyb_cmds.run()
