6d1dd190970623176013956c25d06e29fcf719ae
AndrePatri <andreapatrizi1b6e6@gmail.com>
2024-09-26 20:27:10 +0200

    x2 batch size + smaller critic lr

--- lrhc_control/training_algs/sac/sactor_critic_algo.py
+++ lrhc_control/training_algs/sac/sactor_critic_algo.py
@@ -834,14 +834,14 @@ class SActorCriticAlgoBase():
         self._replay_buffer_size_nominal = int(10e6) # 32768
         self._replay_buffer_size_vec = self._replay_buffer_size_nominal//self._num_envs # 32768
         self._replay_buffer_size = self._replay_buffer_size_vec*self._num_envs
-        self._batch_size = 4096
+        self._batch_size = 8192
         self._total_timesteps = int(tot_tsteps)
         self._total_timesteps = self._total_timesteps//self._env_n_action_reps # correct with n of action reps
         self._total_timesteps_vec = self._total_timesteps // self._num_envs
         self._total_timesteps = self._total_timesteps_vec*self._num_envs # actual n transitions
   
         self._lr_policy = 5e-4
-        self._lr_q = 1e-3
+        self._lr_q = 5e-4
 
         self._discount_factor = 0.99
         self._smoothing_coeff = 0.005
