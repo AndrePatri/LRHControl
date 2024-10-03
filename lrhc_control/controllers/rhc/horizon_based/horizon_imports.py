352ce4ce5352f597cddc78e0338eba70e6ed6d3d
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-10-03 17:50:07 +0200

    cleaned imports

--- lrhc_control/controllers/rhc/horizon_based/horizon_imports.py
+++ lrhc_control/controllers/rhc/horizon_based/horizon_imports.py
@@ -1,18 +1,21 @@
-# horizon stuff
-import horizon.utils.kin_dyn as kd
-from horizon.problem import Problem
-from horizon.rhc.model_description import FullModelInverseDynamics
-from horizon.rhc.taskInterface import TaskInterface
-from horizon.utils import trajectoryGenerator, resampler_trajectory, utils
-from horizon.utils.resampler_trajectory import Resampler
-import horizon.utils.analyzer as analyzer
+# from perf_sleep.pyperfsleep import PerfSleep
+# from control_cluster_bridge.utilities.cpu_utils.core_utils import get_memory_usage
 
 # robot modeling and automatic differentiation
 import casadi_kin_dyn.py3casadi_kin_dyn as casadi_kin_dyn
 import casadi as cs
 
+# horizon stuff
+import horizon.utils.kin_dyn as kd
+from horizon.problem import Problem
+from horizon.rhc.model_description import FullModelInverseDynamics
+from horizon.rhc.taskInterface import TaskInterface
+from horizon.utils import trajectoryGenerator, utils
+# from horizon.utils.resampler_trajectory import Resampler
+# import horizon.utils.analyzer as analyzer
+
+
 # phase managing
 import phase_manager.pymanager as pymanager
 import phase_manager.pyphase as pyphase
 import phase_manager.pytimeline as pytimeline
-
--- lrhc_control/controllers/rhc/horizon_based/hybrid_quad_rhc.py
+++ lrhc_control/controllers/rhc/horizon_based/hybrid_quad_rhc.py
@@ -1,15 +1,13 @@
 from control_cluster_bridge.controllers.rhc import RHController
+# from perf_sleep.pyperfsleep import PerfSleep
+# from control_cluster_bridge.utilities.cpu_utils.core_utils import get_memory_usage
 
 from lrhc_control.controllers.rhc.horizon_based.horizon_imports import * 
 
-from lrhc_control.controllers.rhc.horizon_based.hybrid_quad_rhc_refs import HybridQuadRhcRefs
-from lrhc_control.controllers.rhc.horizon_based.gait_manager import GaitManager
-
-from SharsorIPCpp.PySharsorIPC import VLevel
+# from SharsorIPCpp.PySharsorIPC import VLevel
 from SharsorIPCpp.PySharsorIPC import Journal, LogType
 
 import numpy as np
-from scipy.spatial.transform import Rotation
 
 import os
 # import shutil
