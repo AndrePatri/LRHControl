67d670b48609ef3f35f799bd3ff88d9dd08dfc83
AndrePatri <andreapatrizi1b6e6@gmail.com>
2025-02-23 15:49:58 +0100

    added comments

--- lrhc_control/utils/nn/layer_utils.py
+++ lrhc_control/utils/nn/layer_utils.py
@@ -57,9 +57,14 @@ def llayer_init(layer,
 
         # Apply Layer Normalization or Batch Normalization
         if add_layer_norm:
-            layer = nn.Sequential(layer, nn.LayerNorm(layer.out_features, device=device, dtype=dtype))
+            layer = nn.Sequential(layer, nn.LayerNorm(layer.out_features, device=device, dtype=dtype,
+                                                elementwise_affine=True, bias=True))
         elif add_batch_norm:
-            layer = nn.Sequential(layer, nn.BatchNorm1d(layer.out_features, device=device, dtype=dtype))
+            layer = nn.Sequential(layer, nn.BatchNorm1d(layer.out_features, device=device, dtype=dtype,
+                                                    eps=1e-05, 
+                                                    affine=True, # y(k) = γ(k)̂ x(k) + β(k). to make sure that the transformation inserted in the network can represent the identity transform
+                                                    momentum=0.1, #used if track_running_stats (stats smoothing coeff)
+                                                    track_running_stats=True))
             
         return layer
 
