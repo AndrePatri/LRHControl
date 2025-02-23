fb2e2eb45f2c184fcb31c6ad73dab345ddcc7a4a
AndrePatri <andreapatrizi1b6e6@gmail.com>
2025-02-23 16:04:49 +0100

    fixed other missing import

--- lrhc_control/utils/nn/layer_utils.py
+++ lrhc_control/utils/nn/layer_utils.py
@@ -1,7 +1,7 @@
 import torch
 import math
 from torch.nn.utils import weight_norm, remove_weight_norm    
-from torch.nn import BatchNorm1d, LayerNorm
+from torch.nn import BatchNorm1d, LayerNorm, Sequential
 
 from EigenIPC.PyEigenIPC import LogType
 from EigenIPC.PyEigenIPC import Journal
@@ -59,10 +59,10 @@ def llayer_init(layer,
 
         # Apply Layer Normalization or Batch Normalization
         if add_layer_norm:
-            layer = nn.Sequential(layer, LayerNorm(layer.out_features, device=device, dtype=dtype,
+            layer = Sequential(layer, LayerNorm(layer.out_features, device=device, dtype=dtype,
                                                 elementwise_affine=True, bias=True))
         elif add_batch_norm:
-            layer = nn.Sequential(layer, BatchNorm1d(layer.out_features, device=device, dtype=dtype,
+            layer = Sequential(layer, BatchNorm1d(layer.out_features, device=device, dtype=dtype,
                                                     eps=1e-05, 
                                                     affine=True, # y(k) = γ(k)̂ x(k) + β(k). to make sure that the transformation inserted in the network can represent the identity transform
                                                     momentum=0.1, #used if track_running_stats (stats smoothing coeff)
