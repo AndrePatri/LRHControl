85de63eb3a8b00a718368717574a5afce907a227
AndrePatri <andreapatrizi1b6e6@gmail.com>
2025-02-28 14:34:47 +0100

    deault to big UTD, big replay buffer and decent bsize + mostly frontal tracking

--- lrhc_control/training_algs/sac/sactor_critic_algo.py
+++ lrhc_control/training_algs/sac/sactor_critic_algo.py
@@ -523,16 +523,16 @@ class SActorCriticAlgoBase(ABC):
             custom_args: Dict = {}):
     
         self._collection_freq=1
-        self._update_freq=1
+        self._update_freq=16
 
-        self._replay_buffer_size_vec=3*self._task_rand_timeout_ub # cover at least a number of eps            
+        self._replay_buffer_size_vec=15*self._task_rand_timeout_ub # cover at least a number of eps            
         self._replay_buffer_size = self._replay_buffer_size_vec*self._num_envs
         if self._replay_buffer_size_vec < 0: # in case env did not properly define _task_rand_timeout_ub
             self._replay_buffer_size = int(1e6)
             self._replay_buffer_size_vec = self._replay_buffer_size//self._num_envs
             self._replay_buffer_size=self._replay_buffer_size_vec*self._num_envs
 
-        self._batch_size = 8192
+        self._batch_size = 2048
 
         new_transitions_per_batch=self._collection_freq*self._num_envs/self._replay_buffer_size # assumes uniform sampling
         self._utd_ratio=self._update_freq/(new_transitions_per_batch*self._batch_size)
