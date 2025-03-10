241d724035c64a0ac3a9d70c8ff648eb8c21332a
AndrePatri <andreapatrizi1b6e6@gmail.com>
2025-03-10 21:15:10 +0100

    more reasonable defaults

--- lrhc_control/envs/fake_pos_env_phase_param.py
+++ lrhc_control/envs/fake_pos_env_phase_param.py
@@ -32,8 +32,8 @@ class FakePosEnvPhaseParam(PhaseParametrizationBaseline):
             timeout_ms: int = 60000,
             env_opts: Dict = {}):
 
-        self._add_env_opt(env_opts, "max_distance", default=10.0) # [m] 
-        self._add_env_opt(env_opts, "min_distance", default=3.0) # [m]
+        self._add_env_opt(env_opts, "max_distance", default=5.0) # [m] 
+        self._add_env_opt(env_opts, "min_distance", default=0.0) # [m]
         self._add_env_opt(env_opts, "max_vref", default=0.5) # [m/s]
         self._add_env_opt(env_opts, "max_dp", default=5.0) # [m] after this, v ref saturates
         self._add_env_opt(env_opts, "max_dt", default=env_opts["max_dp"]/ env_opts["max_vref"])
