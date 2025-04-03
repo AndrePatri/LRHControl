39a49565729188e1967c0ee428a996a2b162526e
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2025-04-03 10:24:24 +0100

    higher default max angular vel trgt

--- lrhc_control/envs/linvel_env_baseline.py
+++ lrhc_control/envs/linvel_env_baseline.py
@@ -242,7 +242,7 @@ class LinVelTrackBaseline(LRhcTrainingEnvBase):
         self._add_env_opt(env_opts, "max_linvel_ref", default=0.3) # m/s
         self._add_env_opt(env_opts, "max_angvel_ref", default=0.0) # rad/s
         if env_opts["add_angvel_ref_rand"]:   
-            env_opts["max_angvel_ref"]=0.2
+            env_opts["max_angvel_ref"]=0.4
 
         # ready to init base class
         self._this_child_path = os.path.abspath(__file__)
