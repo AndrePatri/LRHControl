df87265850bbd1380b9a2120349d8932f3188e4c
AndrePatri <andreapatrizi1b6e6@gmail.com>
2025-12-20 00:02:30 +0100

    reverted back to huge batch size, with half utd

--- aug_mpc/training_algs/sac/sactor_critic_algo.py
+++ aug_mpc/training_algs/sac/sactor_critic_algo.py
@@ -555,7 +555,7 @@ class SActorCriticAlgoBase(ABC):
             custom_args: Dict = {}):
     
         self._collection_freq=1
-        self._update_freq=8
+        self._update_freq=4
 
         self._replay_buffer_size_vec=10*self._task_rand_timeout_ub # cover at least a number of eps            
         self._replay_buffer_size = self._replay_buffer_size_vec*self._num_envs
@@ -564,7 +564,7 @@ class SActorCriticAlgoBase(ABC):
             self._replay_buffer_size_vec = self._replay_buffer_size//self._num_envs
             self._replay_buffer_size=self._replay_buffer_size_vec*self._num_envs
 
-        self._batch_size = 8192
+        self._batch_size = 16394
 
         new_transitions_per_batch=self._collection_freq*self._num_envs/self._replay_buffer_size # assumes uniform sampling
         self._utd_ratio=self._update_freq/(new_transitions_per_batch*self._batch_size)
